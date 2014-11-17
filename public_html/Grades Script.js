e4930b6025ef993de9fe080cb5f5cf18f38e9a47
Michael Remediakis <m_remedi@yahoo.com>
2014-11-16 20:34:49 -0600

    Finished the Grades Page mostly

--- public_html/Grades Script.js
+++ public_html/Grades Script.js
@@ -1,13 +1,12 @@
 
 $(document).ready(function() {
-  //  $(".notificationbar").addClass("enter");
+    $(".notificationbar").addClass("enter");
     $('#exitbutton').click(function () {
         $('.notificationbar').removeClass("enter");
     });
     $('.sidebar').hover(function() {
         $('.notificationbar').toggleClass("shrink");
-        $('.classcontainer2').toggleClass("shrink");
-        $('.classcontainer2 ul li').toggleClass("shrink");
+        $('#classes').toggleClass("small");
     });
 });
 
