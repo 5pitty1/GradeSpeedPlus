ae65dff61f12026a0d132ddba0948848058c8b18
Michael Remediakis <m_remedi@yahoo.com>
2014-11-18 09:54:42 -0600

    Created a Class Grades Page + Fixed Code Formatting
    I also added a color coding on the grades page

---
+++ public_html/Global Script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".notificationbar").addClass("enter");
+    $('#exitbutton').click(function () {
+        $('.notificationbar').removeClass("enter");
+    });
+    $('.sidebar').hover(function () {
+        $('.notificationbar').toggleClass("shrink");
+    });
+});
---
+++ public_html/Grades Page/Grades Script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $('.sidebar').hover(function () {
+        $('#classes').toggleClass("small");
+    });
+})
