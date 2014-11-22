67ec10d27288bf059bc6a89f20dd95038cb451e5
Michael Remediakis <m_remedi@yahoo.com>
2014-11-22 15:10:27 -0600

    Continuing Work On The Class Page

--- public_html/Grades Page/Grades Script.js
+++ public_html/Grades Page/Grades Script.js
@@ -2,4 +2,23 @@ $(document).ready(function () {
     $('.sidebar').hover(function () {
         $('#classes').toggleClass("small");
     });
-})
+    $('#classes').find('tr').each(function() {
+        var grade = $(this).find('td').html();
+        $(this).find('td').each(function() {
+            grades(grade);
+        });
+    });
+});
+
+function grades(x){
+    x = parseInt(x);
+    if(x<70){
+        $(this).addClass("F");
+    } else if(x>=70&&x<80) {
+        $(this).addClass("C");
+    } else if(x>=80&&x<90) {
+        $(this).addClass("B");
+    } else {
+        $(this).addClass("A");
+    }
+}
